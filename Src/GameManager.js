509537213367f39bdd3898e341bcc31b32c23632
ankitkumar782 <akankit615@gmail.com>
2025-10-01 13:21:36 +0530

    ok

--- Src/GameManager.js
+++ Src/GameManager.js
@@ -1,5 +1,54 @@
 const { Newgame, Makemove } = require("./Newgame");
 
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/tictactoe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Define pending user schema
+const pendingUserSchema = new mongoose.Schema({
+    socketId: String,
+    joinedAt: { type: Date, default: Date.now }
+});
+
+// Create model
+const PendingUser = mongoose.model('PendingUser', pendingUserSchema);
+
+// Function to add pending user to DB
+async function addPendingUserToDB(socket) {
+    try {
+        const pendingUser = new PendingUser({
+            socketId: socket.id
+        });
+        await pendingUser.save();
+    } catch (err) {
+        console.error('Error saving pending user:', err);
+    }
+}
+
+// Function to remove pending user from DB
+async function removePendingUserFromDB(socket) {
+    try {
+        await PendingUser.deleteOne({ socketId: socket.id });
+    } catch (err) {
+        console.error('Error removing pending user:', err);
+    }
+}
+
+// Function to get first pending user
+async function getFirstPendingUser() {
+    try {
+        return await PendingUser.findOne().sort({ joinedAt: 1 });
+    } catch (err) {
+        console.error('Error getting pending user:', err);
+        return null;
+    }
+}
+
+
 var user=[];
 var pendinguser=[];
 var Game=[];
@@ -24,6 +73,7 @@ exports.removeuser=(socket)=>{
             
             if(pendinguser.length>0&&pendinguser[0]!='empty'){
                 //start a game
+                console.log(pendinguser.length)
                 const startgame=Newgame(pendinguser[0],socket);
                 Game.push(startgame);
                 
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-
 const WebSocketServer = require('ws').Server;
-
 const wss = new WebSocketServer({ port: 1002 });
+
 const GameManager = require('./Src/GameManager')
+const app=require('./app')
 
-
-
+app.listen(3000,()=>{
+    console.log("server started")
+})
 
 
 wss.on('connection', function connection(ws) {
 
+
+    // setInterval(() => {
+    //      ws.send("hello")
+    // }, 2000);
+   
     GameManager.adduser(ws);
     //   ws.on('message', function message(data) {
     //     console.log('received: %s', data);
@@ -19,3 +25,4 @@ wss.on('connection', function connection(ws) {
 
     
 });
+
